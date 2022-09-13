d22e63e43971ab2535ba3f3a1228dc892fb84721
Danil Mirosh <miroshnichenko.da.vi@mail.ru>
2022-09-14 00:56:25 +0500

    add file

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,15 @@ import (
 	"log"
 	"todo"
 	"todo/pkg/handler"
+	"todo/pkg/repository"
+	"todo/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	svr := new(todo.Server)
 	if err := svr.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while ruuning http server: %s", err.Error())
